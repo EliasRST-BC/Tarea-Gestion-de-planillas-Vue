ed045254745b1fe3c93eedc24ab5b25e0c55808c
Elias.S <EliasTroche2004@gmail.com>
2024-11-07 17:08:08 -0300

    modified vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/Tarea-Gestion-de-planillas-Vue/'
-  : '/'
 
-})
+module.exports = defineConfig({
+  transpileDependencies: [],  // Cambia esto a un array vacío
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Tarea-Gestion-de-planillas-Vue/'
+    : '/'
+  })
